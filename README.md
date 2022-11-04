58649475723d19c1e39a81ad7eef3b883d52d9c6
Metal <50769245+Metal-666@users.noreply.github.com>
2022-11-04 05:21:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# data_yeeter_flutter
+ This is a Flutter client for data_yeeter, my second university project.
---
+++ android/app/src/main/kotlin/com/example/data_yeeter_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.data_yeeter_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
