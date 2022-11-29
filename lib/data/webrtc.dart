5e2ff1ebe62e061f455ee68669642c9a0786ac34
Metal <50769245+Metal-666@users.noreply.github.com>
2022-11-29 02:46:42 +0200

    I already hate webrtc

--- lib/data/webrtc.dart
+++ lib/data/webrtc.dart
@@ -11,10 +11,19 @@ const Map<String, dynamic> _configuration = {
   ]
 };
 
+const Map<String, dynamic> _constraints = {
+  'mandatory': {
+    'OfferToReceiveAudio': false,
+    'OfferToReceiveVideo': false,
+  },
+  'optional': [],
+};
+
 class WebRTCConnection {
   final void Function(List<int> chunk) onFileChunk;
 
   late final RTCPeerConnection _peerConnection;
+  late final RTCDataChannel _fileChannel;
 
   WebRTCConnection(this.onFileChunk);
 
@@ -30,37 +39,45 @@ class WebRTCConnection {
     _peerConnection.onIceConnectionState = (final state) {
       log('ICE Connection State changed to: ${state.toString()}');
     };
+    _peerConnection.onConnectionState = (final state) {
+      log('Connection State changed to: ${state.toString()}');
+    };
+    _peerConnection.onSignalingState = (final state) {
+      log('Signalling State changed to: ${state.toString()}');
+    };
   }
 
   Future<String?> createOffer() async {
-    final RTCSessionDescription description =
-        await _peerConnection.createOffer();
+    _fileChannel =
+        await _peerConnection.createDataChannel('file', RTCDataChannelInit());
 
-    if (description.sdp == null) {
-      return null;
-    }
+    final RTCSessionDescription description =
+        await _peerConnection.createOffer(_constraints);
 
     log('Offer was created!');
+    log(description.sdp ?? '');
 
     await _peerConnection.setLocalDescription(description);
 
-    return description.sdp!;
+    return description.sdp;
   }
 
   Future<String?> createAnswer(final String offer) async {
+    _peerConnection.onDataChannel = (final channel) {
+      if (channel.label == 'file') {
+        channel.onMessage = (final data) => onFileChunk(data.binary);
+      }
+    };
+
     final RTCSessionDescription offerDescription =
         RTCSessionDescription(offer, 'offer');
-
-    if (offerDescription.sdp == null) {
-      return null;
-    }
 
     log('Offer was received!');
 
     await _peerConnection.setRemoteDescription(offerDescription);
 
     final RTCSessionDescription answerDescription =
-        await _peerConnection.createAnswer();
+        await _peerConnection.createAnswer(_constraints);
 
     if (answerDescription.sdp == null) {
       return null;
@@ -70,23 +87,7 @@ class WebRTCConnection {
 
     await _peerConnection.setLocalDescription(answerDescription);
 
-    _peerConnection.onDataChannel = (final channel) {
-      switch (channel.label) {
-        case 'control':
-          {
-            //controlChannel = channel;
-            break;
-          }
-        case 'file':
-          {
-            //fileChannel = channel;
-            channel.onMessage = (final data) => onFileChunk(data.binary);
-            break;
-          }
-      }
-    };
-
-    return answerDescription.sdp!;
+    return answerDescription.sdp;
   }
 
   Future<void> startStream(
@@ -96,26 +97,21 @@ class WebRTCConnection {
     final RTCSessionDescription description =
         RTCSessionDescription(answer, 'answer');
 
-    if (description.sdp == null) {
-      return;
-    }
-
     log('Answer was received!');
+    log(answer);
 
     await _peerConnection.setRemoteDescription(description);
-
-    final RTCDataChannel fileChannel =
-        (await _peerConnection.createDataChannel('file', RTCDataChannelInit()));
 
     await File(filePath).openRead().listen((final event) {
       onFileChunk(event);
 
-      fileChannel
+      _fileChannel
           .send(RTCDataChannelMessage.fromBinary(Uint8List.fromList(event)));
     }).asFuture();
   }
 
   Future<void> dispose() async {
     await _peerConnection.dispose();
+    await _fileChannel.close();
   }
 }
